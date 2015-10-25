d3b03646dee5b24835427f90b9a9782b097da661
jkuk <j_kuk@post.massbay.edu>
2015-10-25 01:33:39 -0400

    removing a bunch of files that were moved to a different folder

--- compiler/compiler.bat
+++
@@ -1,7 +0,0 @@
-del *.class
-javac Lexer.java
-java Lexer ..\input\KleeneSourceCode.kd ..\input\KleeneSymbols.txt -verbose
-java Lexer ..\input\AlternateSourceCode.kd ..\input\AlternateSymbols.txt -verbose
-java Lexer ..\input\ConcatenateSourceCode.kd ..\input\ConcatenateSymbols.txt -verbose
-java Lexer ..\input\TestSourceCode.kd ..\input\TestSymbols.txt -verbose
-java Lexer ..\input\SourceCode.kd ..\input\Symbols.txt -verbose
